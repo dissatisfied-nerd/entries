6b51a37c015bee2b70e70e374b59c92806dfc86b
axr-pekarnya <10b1111nika@gmail.com>
2025-06-29 09:04:01 +0300

    Done

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 jupyter nbconvert --to notebook --execute generate.ipynb --inplace --ExecutePreprocessor.kernel_name=venv
 jupyter nbconvert --to notebook --execute migrate.ipynb --inplace --ExecutePreprocessor.kernel_name=venv
-jupyter nbconvert --to notebook --execute etl.ipynb --inplace --ExecutePreprocessor.kernel_name=venv
+jupyter nbconvert --to notebook --execute intervals_tgt.ipynb --inplace --ExecutePreprocessor.kernel_name=venv
+jupyter nbconvert --to notebook --execute workdays_tgt.ipynb --inplace --ExecutePreprocessor.kernel_name=venv
+jupyter nbconvert --to notebook --execute aggregated_info_tgt.ipynb --inplace --ExecutePreprocessor.kernel_name=venv
+jupyter nbconvert --to notebook --execute visual.ipynb --inplace --ExecutePreprocessor.kernel_name=venv
